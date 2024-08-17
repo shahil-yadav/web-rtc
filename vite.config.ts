a42688b632216b50bdc1f26803c454a5ab80f49a
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-08-17 10:42:25 +0530

    plugin integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { comlink } from 'vite-plugin-comlink'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [comlink(), react(), tsconfigPaths()],
+  worker: {
+    plugins: [comlink()],
+  },
   build: {
     rollupOptions: {
       output: {
