273e18f18c9d5687543ad70c7cf4ea879b7d367c
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-08-18 11:24:49 +0530

    added intergration of thread

---
+++ src/lib/workers.ts
@@ -0,0 +1,6 @@
+import { fetchRandomAvatar } from '~/components/root/utils/storage'
+import { setupFirebase } from './firebase'
+
+setupFirebase()
+
+export { fetchRandomAvatar }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { comlink } from 'vite-plugin-comlink'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [comlink(), react(), tsconfigPaths()],
+  worker: {
+    plugins: [comlink()],
+  },
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: resolve(__dirname, './src'),
+      },
+    ],
+  },
   build: {
     rollupOptions: {
       output: {
